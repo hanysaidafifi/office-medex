e7cd5d3094c13cc5cde572695e9c9ff7ca7b5cc0
Hany Afifi <hanysaidafifi@gmail.com>
2023-07-12 15:47:55 -0700

    started the front

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import promoOfficeMedex from './assets/promo-officemedex.png'
+import advpsyLogo from './assets/advancedpsych-logoonly.png'
 import './App.css'
 
 function App() {
@@ -9,25 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://advancedpsychiatryassociates.com/" target="_blank">
+          <img src={promoOfficeMedex} className="logo" alt="Office Medex promo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Office Medex</h1>
+
     </>
   )
 }
